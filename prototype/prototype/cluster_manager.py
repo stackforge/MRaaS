088bba9e909a6a90e02cc58b5bc020a5d40c1514
Tim Miller <tim.miller.0@gmail.com>
2012-03-16 16:52:54 +0000

    add cli script.
    Change-Id: I9db6d56a6da2a16bb6db204e9681c8da25923f82

---
+++ prototype/prototype/cluster_manager.py
@@ -0,0 +1,48 @@
+import os
+
+import novaclient.v1_1
+
+import hadoop_cluster
+import config
+
+home = os.getenv('HOME')
+state_dir = home + '/.hadoop_clusters/'
+if not os.path.exists(state_dir):
+    os.mkdir(state_dir)
+
+
+def new_cluster_name():
+    n = 0
+    cluster_dir = state_dir + 'cluster' + str(n) + '/'
+    while os.path.exists(cluster_dir):
+        n = n + 1
+        cluster_dir = state_dir + 'cluster' + str(n) + '/'
+    return 'cluster' + str(n)
+
+
+def nova_client():
+    return novaclient.v1_1.client.Client(config.name, config.password, config.project_id, config.auth_url, service_type='compute')
+
+def create_cluster(options):
+    if not options.name:
+        options.name = new_cluster_name()
+    cluster_dir = state_dir + options.name + '/'
+    os.mkdir(cluster_dir)
+    cluster = hadoop_cluster.HadoopCluster(nova_client(), options.name, cluster_dir)
+    cluster.provision(config.num_data_nodes, config.flavor_name, config.base_image_name, config.hadoop_image_name)
+    cluster.to_file()
+
+def list_clusters():
+    for dname in sorted(os.listdir(state_dir)):
+        d = state_dir + dname
+        cluster = hadoop_cluster.HadoopCluster(nova_client(), dname, d)
+        cluster.from_file()
+        cluster.print_info()
+
+def destroy_cluster(name):
+    cluster_dir = state_dir + name + '/'
+    if not os.path.exists(cluster_dir):
+        raise Exception('unknown cluster : ' + name)
+    cluster = hadoop_cluster.HadoopCluster(nova_client(), name, cluster_dir)
+    cluster.from_file()
+    cluster.destroy()
